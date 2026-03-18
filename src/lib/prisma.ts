80e80cd9ba74a5d868f4499f5bd20704570b9091
khactrinh <khactrinhcn02@gmail.com>
2026-03-18 13:31:37 +0700

    Switch database from SQLite to PostgreSQL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const getPrisma = () => {
-  if (globalForPrisma.prisma) return globalForPrisma.prisma;
-  
-  const adapter = new PrismaBetterSqlite3({ url: "file:./prisma/dev.db" });
-  return new PrismaClient({ adapter, log: ["error"] });
-}
-
-export const prisma = getPrisma();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
